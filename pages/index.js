41c47273c79984347e3f563ce5a90e4755e9af27
sugoi-reirei <sugoikaizen.kanaya@gmail.com>
2021-10-19 15:29:46 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,139 @@
 import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
+import Link from 'next/link'
+import Date from '../components/date'
+
+
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      
+      <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="card"
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+
+          <a
+            href="https://omocoro.jp/"
+            className="card"
+          >
+            <h3>オモコロ &rarr;</h3>
+            <p>
+              暇つぶしにピッタリの漫画や記事を毎日のように配信しています。いくら読んでも無料、そしていくら読んでも頭がよくなりません。
+            </p>
+          </a>
+
+          <a
+            href="https://dailyportalz.jp/"
+            className="card"
+          >
+            <h3>デイリーポータルZ &rarr;</h3>
+            <p>
+              面白いだけかと思ったらちょっと役に立つこともある。これは役に立つかもと思って読むとそうでもない。そんなオルタナティブなポータルサイトです。
+            </p>
+          </a>
+        </div>
+        <footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel" className="logo" />
+          </a>
+        </footer>
+    </Layout>
+  )
+}
+/*
+import Head from 'next/head'
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Created My Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -43,6 +166,26 @@ export default function Home() {
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+
+          <a
+            href="https://omocoro.jp/"
+            className="card"
+          >
+            <h3>オモコロ &rarr;</h3>
+            <p>
+              暇つぶしにピッタリの漫画や記事を毎日のように配信しています。いくら読んでも無料、そしていくら読んでも頭がよくなりません。
+            </p>
+          </a>
+
+          <a
+            href="https://dailyportalz.jp/"
+            className="card"
+          >
+            <h3>デイリーポータルZ &rarr;</h3>
+            <p>
+              面白いだけかと思ったらちょっと役に立つこともある。これは役に立つかもと思って読むとそうでもない。そんなオルタナティブなポータルサイトです。
             </p>
           </a>
         </div>
@@ -207,3 +350,4 @@ export default function Home() {
     </div>
   )
 }
+*/
